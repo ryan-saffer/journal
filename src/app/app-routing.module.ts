29907ce327c05e39b6dc966fb2a2998edd368969
Ryan Saffer <ryansaffer@gmail.com>
2022-04-30 18:51:21 +1000

    Init journal component
    Point to the new component as the home route.
    Kept the ionic tabs template around to learn from - set up to route '/tabs/'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { JournalComponent } from './journal/journal.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: JournalComponent
+  },
+  {
+    path: 'tabs',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   }
 ];
