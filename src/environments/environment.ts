260574a1a6af29026d45de12e24e29c54443f20a
Ryan Saffer <ryansaffer@gmail.com>
2022-04-30 20:00:49 +1000

    Init firebase project
    Deployed initial app to firebase: https://ng-journal-cddb9.web.app/

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCnrddfme_U_zPxlx9uTFIgnbfw9wEOYyg",
+    authDomain: "ng-journal-cddb9.firebaseapp.com",
+    projectId: "ng-journal-cddb9",
+    storageBucket: "ng-journal-cddb9.appspot.com",
+    messagingSenderId: "703343897476",
+    appId: "1:703343897476:web:8d480aad48fa71102b0bd1",
+    measurementId: "G-0W9PFC2E2C"
+  }
 };
 
 /*
